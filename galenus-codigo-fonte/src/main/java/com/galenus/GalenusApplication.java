0a16c79987b94657e65b1bc92476d4b9a994750e
patrik <patrikmelod@gmail.com>
2023-05-02 19:11:26 -0300

    exemplo de main app

--- galenus-codigo-fonte/src/main/java/com/galenus/GalenusApplication.java
+++ galenus-codigo-fonte/src/main/java/com/galenus/GalenusApplication.java
@@ -5,9 +5,41 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 @SpringBootApplication
 public class GalenusApplication extends JFrame {
+
+    public GalenusApplication() {
+        initUI();
+    }
+
+    private void initUI() {
+        var quitButton = new JButton("Quit");
+        quitButton.addActionListener((ActionEvent event) -> {
+            System.exit(0);
+        });
+
+        createLayout(quitButton);
+        setTitle("Quit button");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void createLayout(JComponent... arg) {
+        var pane = getContentPane();
+        var gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+    }
 
     public static void main(String[] args) {
         var ctx = new SpringApplicationBuilder(GalenusApplication.class)
